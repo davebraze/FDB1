00efc66db9d3edf23a8245202fad1f4da37c1be9
Dave Braze <davebraze@gmail.com>
2014-08-15 12:08:18 -0400

    added qqp() and qqplot()

---
+++ R/qqplot.R
@@ -0,0 +1,44 @@
+library(ggplot2)
+library(gridExtra)
+
+##' Create a QQ plot for a variable within a data.frame.
+##'
+##' Given a data.frame and the name of a variable within it create a quantile-quantile plot based on
+##' the normal distribution. Also plot a 'fit' line with intercept equal to sample mean and slope
+##' equal to sample standard deviation.
+##' @title FDB1::qqp()
+##' @param v a character string naming a variable in data.frame 'dat'
+##' @param dat a data.frame containing at least 1 variable 'v'
+##' @return a ggplot2 object
+##' @author Dave Braze
+##' @export
+qqp <- function(v, dat) {
+    mn <- mean(dat[[v]], na.rm=T) # maybe add mean and sd to lower-right corner of plot?
+    sd <- sd(dat[[v]], na.rm=T)
+    p <- ggplot(dat, aes_string(sample=v))  # NOTE: use of aes_string() in place of aes()
+    p <- p+geom_point(stat="qq")
+    p <- p+geom_abline(intercept = mn, slope = sd,
+                       size=1.25, col="blue", alpha=1/3)
+    p <- p+coord_fixed(ratio=1/sd)  # TODO: better to do diff(range(sample))/(diff(range(sample)) / s)
+    p <- p+ggtitle(v) # TODO: drop title and add label to upper-left corner of plot
+    return(p)
+}
+
+##' Create and plot a QQ plot matrix
+##'
+##' Given a data.frame and vector of variable names within it, create a matrix of quantile-quantile
+##' plots based on the normal distribution and include 'fit' lines on each with intercepts equal to
+##' sample means and slopes equal to sample standard deviations.
+##' @title FDB1::qqplot()
+##' @param vars a character vector specifying variables with data.frame 'dat' to plot.
+##' @param dat a data.frame containing variables to be plotted.
+##' @return a grid grob created by applying grid.arrange to list of ggplot2 objects
+##' @seealso \code{\link{qpp}}
+##' @author Dave Braze
+##' @export
+qqplot <- function(vars, dat) {
+    plist <- lapply(vars, FUN=qqp, dat=dat)
+    do.call(grid.arrange, plist)
+    ## qqpm <- do.call(grid.arrange, plist)
+    ## return(qqpm)
+}
