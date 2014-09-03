85781b8ab75304627208d1d260c677ad0516b99a
Dave Braze <davebraze@gmail.com>
2014-09-02 21:34:11 -0400

    Added 'diag' argument to corrTable()

--- R/corrTable.R
+++ R/corrTable.R
@@ -10,23 +10,30 @@
 ##' @return A matrix with R values in the lower triangle and NAs elsewhere
 ##' @author Dave Braze \email{davebraze@@gmail.com}
 ##' @export
-corrTable <- function(M, type, file="") {
+corrTable <- function(M, type='pearson', file="", diag=FALSE) {
 
-    rcor2 <- Hmisc::rcorr(as.matrix(M), type="pearson")
+    rcor2 <- Hmisc::rcorr(as.matrix(M), type=type)
     retval <- rcor2$r
     retval[upper.tri(retval, diag=TRUE)] <- NA
 
     colnames(retval) <- as.character(1:length(colnames(retval)))
     cnames=colnames(retval)
     cnames[1] = paste(",", cnames[1], sep="")
+    colnames(retval) <- cnames
 
-    rownames(retval) <- paste(as.character(1:length(rownames(retval))), vars, sep=". ")
-    retval <- retval[,-dim(retval)[2]]
+    rownames(retval) <- paste(as.character(1:length(rownames(retval))), rownames(retval), sep=". ")
+
+    if("sd"==diag){
+        diag(retval) <- sapply(D2, sd, na.rm=T)
+    } else if ("var"==diag) {
+        diag(retval) <- sapply(D2, var, na.rm=T)
+    } else if ("R"==diag) {
+        diag(retval) <- 1
+    }
 
     if(""!=file) {
         write.table(retval, file=file, sep=",",
-                    col.names=cnames[-length(cnames)], na="", quote=FALSE)
+                    col.names=TRUE, na="", quote=FALSE)
     }
-
     retval
 }
