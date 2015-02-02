b8016e797b50f3f73693e42a84410fa3609dcdeb
Dave Braze <davebraze@gmail.com>
2015-02-02 16:04:18 -0500

    properly handle NAs in is.numeral()

--- R/is.numeral.R
+++ R/is.numeral.R
@@ -1,9 +1,20 @@
 ##' Checks whether each element in a vector of strings can be coerced to numeric.
 ##' @title Checks element-wise if character vector can be coerced to numeric.
 ##' @param c : a vector of strings
-##' @return a vector of logicals
-##' @author cribbed from \url{http://rosettacode.org/wiki/Determine_if_a_string_is_numeric#R}
+##' @return a vector of logicals, NA where is.na(c)
+##' @author Basics cribbed from \url{http://rosettacode.org/wiki/Determine_if_a_string_is_numeric#R}
+##' @author David Braze \email{davebraze@@gmail.com}
 ##' @export
 is.numeral <- function(c) {
-    suppressWarnings(!is.na(as.numeric(c)))
+    retval <- suppressWarnings(!is.na(as.numeric(c)))
+    retval[is.na(c)] <- NA
+    retval
 }
+
+if(FALSE){
+    x <- c('a', 'IX', 'twelve', '0001', '2', '3e1', '-4', '5.5', NA, 'Inf')
+    data.frame(x,
+               is.numeral(x),
+               as.numeric(x))
+}
+
